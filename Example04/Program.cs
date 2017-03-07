bdd47434a4f5b57c5f4cf34b90f8fd9070004beb
wood0706 <wood0706@gmail.com>
2017-03-07 21:26:15 +0800

    數字轉換 有作錯誤處理

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -10,11 +10,28 @@ namespace Example04
     {
         private static void Main(string[] args)
         {
+            int number1 = 0;
+            int number2 = 0;
             Console.Write("請輸入第一個數字:");
-            int number1 = int.Parse(Console.ReadLine());
-            Console.Write("請輸入第二個數字");
-            int number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("{0} +{1} = {2}", number1, number2, number1 + number2);
+            try
+            {
+                number1 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.Write("請輸入第二個數字:");
+            try
+            {
+                number2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
         }
     }
 }
