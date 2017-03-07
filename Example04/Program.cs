42cb05f9bab1f7cd1b6fed02b79bb5c0cd653e60
wood0706 <wood0706@gmail.com>
2017-03-07 21:44:34 +0800

    數字轉換加入錯誤訊息

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -19,7 +19,9 @@ namespace Example04
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("第一個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
             }
 
             Console.Write("請輸入第二個數字:");
@@ -29,7 +31,9 @@ namespace Example04
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("第二個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
             }
             Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
         }
